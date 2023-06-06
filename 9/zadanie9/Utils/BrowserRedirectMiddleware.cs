03667f5c07f0466a3c3dda978fe47847b5b18825
Maciej Borys <maciekjeremi@wp.pl>
2023-06-06 18:41:07 +0200

    sprawko 9

---
+++ 9/zadanie9/Utils/BrowserRedirectMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+
+namespace Utils
+{
+    public class BrowserRedirectMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BrowserRedirectMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var userAgent = context.Request.Headers[HeaderNames.UserAgent].ToString();
+
+            if (userAgent.Contains("Edge") || userAgent.Contains("Edg") || userAgent.Contains("Trident"))
+            {
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ 9/zadanie9/Utils/BrowserRedirectMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Utils
+{
+    public static class BrowserRedirectMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBrowserRedirectMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<BrowserRedirectMiddleware>();
+        }
+    }
+}
